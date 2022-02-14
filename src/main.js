ea0137e68a250ac5a3776357eea42eaa094dc2b4
Yesiblato <87024446+Yesiblato@users.noreply.github.com>
2022-02-14 17:34:29 -0500

    Cambio de paginas

--- src/main.js
+++ src/main.js
@@ -1,6 +1,25 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+//import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+import data from './data/lol/lol.js';
+
+const datos = data.data;
+let arrayDatos = Object.values(datos); // Data convertida a array (object.values)--- object.key
+
+/*let arrayAssassin = arrayDatos.filter(dat => {
+    if (dat == Assassin)
+    return 
+})*/
+
+
+console.log(arrayDatos);
+
+// Cambio de paginas - boton campeones
+document.getElementById("campeonesbtn").addEventListener("click", hidePages)
+
+function hidePages(){
+    document.getElementById('pagina1').style.display = 'none';
+    document.getElementById('pagina2').style.display = 'block';
+ }
